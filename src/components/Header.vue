7a7a96d46ed8079a16f40adc412581448477198b
FrancescoCina <francesco.cina04@gmail.com>
2021-09-10 11:55:01 +0200

    Importato Bootstrap e fatto Header graficamente

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,17 @@
 <template>
-  <header></header>
+  <header>
+    <div class="h-100 container">
+      <div class="row h-100 justify-content-between align-items-center">
+        <div class="col">LOGO</div>
+        <div class="col">
+          <div class="input-search">
+            <input placeholder="Cerca il tuo contenuto" type="search" />
+            <button>Cerca</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
